5e42be353869bc34b91bb7c85b375e52de8ab86f
Liam Cosgrove <lcos159@gmail.com>
2022-10-06 21:25:35 -0500

    removed printing to console on function and made aero sweep work

--- LapSimAeroSweep.m
+++ LapSimAeroSweep.m
@@ -1,10 +1,12 @@
-clear all
+%%% LapSim Aero Sweep
+
+clear all;
 clc
 
 % Vechicle Paramaters
 
 %% Section 1: Vehicle Architecture
-disp('Loading Vehicle Characteristics')
+%disp('Loading Vehicle Characteristics')
 % These are the basic vehicle architecture primary inputs:
 LLTD = 51.5; % Front lateral load transfer distribution (%)
 W = 660; % vehicle + driver weight (lbs)
@@ -15,7 +17,7 @@ twf = 46/12; % front track width (ft)
 twr = 44/12; % rear track width (ft)
 
 %% Section 2: Input Suspension Kinematics
-disp('Loading Suspension Kinematics')
+%disp('Loading Suspension Kinematics')
 % this section is actually optional. So if you set everything to zero, you
 % can essentially leave this portion out of the analysis. Useful if you are
 % only trying to explore some higher level relationships
@@ -38,48 +40,65 @@ casterf = 0; % front caster angle (deg)
 KPIf = 0; % front kingpin inclination angle (deg)
 casterr = 4.1568; % rear caster angle (deg)
 KPIr = 0; % rear kingpin inclination angle (deg)
-%% Section 3: Input Aero Parameters
-disp('Loading Aero Model')
 
-% Cl = linspace(.03,.6,2);
-Cl = .0418; %279/418
-% Cd = linspace(.01,.03,2);
-Cd = .0184; % .024
+%% Section 3: Input Aero Parameters
+%disp('Loading Aero Model')
+
 CoP = 48; % front downforce distribution (%)
+
+CLA = [-2.9:0.6:0.1] * -0.3345 * 0.03824; % Lift equation without Velocity component
+
+cdOffsetM = [0:0.1:0.4] * 0.3345 * 0.03824;
+
+k = 1;
+
+IterTotal = length(CLA) * length(cdOffsetM);
+
+fprintf('Running %.0f iterations.\n', IterTotal);
+
+for j = 1:length(cdOffsetM)
+    
+    cdOffset = cdOffsetM(j);
+    
+for i = 1:length(CLA)
+
+    Cl = CLA(i);
+    Cd = 0.24 * Cl + (0.3 * 0.3345 * 0.03824) + cdOffset;
 
 LapSimOutput = LapSim(LLTD, W, WDF, cg, l, twf, twr, rg_f, rg_r,pg, WRF, WRR, IA_staticf, IA_staticr, IA_compensationr, IA_compensationf, casterf, KPIf, casterr, KPIr, Cl, Cd, CoP);
 
-distance = LapSimOutput.distance;
-velocity = LapSimOutput.velocity;
-acceleration = LapSimOutput.acceleration;
-lateral_accel = LapSimOutput.lateral_accel;
-distance_ax = LapSimOutput.distance_ax;
-velocity_ax = LapSimOutput.velocity_ax;
-acceleration_ax = LapSimOutput.acceleration_ax;
-lateral_accel_ax = LapSimOutput.lateral_accel_ax;
-%% Section 19: Plot Results
-disp('Plotting Results')
-% This is just to make some pretty pictures, feel free to comment this out
-figure
-plot(distance,velocity,'k')
-title('Endurance Simulation Velocity Trace')
-xlabel('Distance Travelled (d) [ft]')
-ylabel('Velocity (V) [ft/s]')
-figure
-plot(distance,acceleration,distance,lateral_accel)
-title('Endurance Simulation Acceleration Traces')
-xlabel('Distance Travelled (d) [ft]')
-ylabel('Acceleration [g]')
-legend('Longitudinal','Lateral')
-figure
-plot(distance_ax,velocity_ax,'k')
-title('Autocross Simulation Velocity Trace')
-xlabel('Distance Travelled (d) [ft]')
-ylabel('Velocity (V) [ft/s]')
-figure
-plot(distance_ax,acceleration_ax,distance_ax,lateral_accel_ax)
-title('Autocross Simulation Acceleration Traces')
-xlabel('Distance Travelled (d) [ft]')
-ylabel('Acceleration [g]')
-legend('Longitudinal','Lateral')
-disp('Analysis Complete')
+T_axismax = max(LapSimOutput.time_elapsed);
+
+ClaM(k) = Cl;
+CdaM(k) = Cd;
+TimeM(k) = T_axismax;
+
+clear LapSimOutput
+
+fprintf('Completed iteration %.0f of %.0f.\n', k, IterTotal);
+
+k = k + 1;
+
+end
+
+end
+
+figure(3)
+xlin = linspace(min(ClaM), max(ClaM), 100);
+ylin = linspace(min(CdaM), max(CdaM), 100);
+[X,Y] = meshgrid(xlin, ylin);
+Z = griddata(ClaM,CdaM,TimeM,X,Y,'v4');
+[dfdx, dfdy] = gradient(Z);
+s = mesh(X/(-0.3345 * 0.03824),Y/(0.3345 * 0.03824),Z, sqrt(dfdx.^2 + dfdy.^2));
+colormap(turbo);
+axis tight; hold on
+plot3(ClaM/(-0.3345 * 0.03824),CdaM/(0.3345 * 0.03824),TimeM,'m.','MarkerSize',5)
+s.FaceColor = 'interp';
+s.EdgeColor = 'none';
+xlabel('Cla')
+ylabel('Cda')
+zlabel('Time(s)')
+g = colorbar;
+title(g, 'gradient')
+clim([0 0.1])
+hold off
