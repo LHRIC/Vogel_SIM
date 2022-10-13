fb5567e060dbe7c8942a54b304a78b3f2fe211b3
Chef <98376873+remyferu@users.noreply.github.com>
2022-10-13 14:55:38 -0500

    Debugging Init Commit

--- Final Drive Ratio/FinalDriveCalculator.m
+++ Final Drive Ratio/FinalDriveCalculator.m
@@ -3,41 +3,40 @@ close all;
 clear all;
 clc;
 
-g = 9.8;
+g = 9.81;
 
 %% Car Specs
 
 % Vehicle Specs
-    Rt = 0.0825; % wheel radius
-    redline = 12000;
+Rt = 0.2032; % wheel radius
+redline = 12000; % desired maximum rpm
 
-% Engine Data Import 
-    engine_torque_data = readmatrix('Engine Data');
-    engine_gear_ratio = [33/12, 32/16, 30/18, 26/18, 30/23, 29/24];
+% Engine Data 
+engine_torque_data = readmatrix('Engine Data');
+engine_gear_ratio = sort([33/12, 32/16, 30/18, 26/18, 30/23, 29/24]); % 
 
-    figure();
-        plot(engine_torque_data(:,1), engine_torque_data(:,2))
+% Plots Engine Data
+figure();
+    plot(engine_torque_data(:,1), engine_torque_data(:,2))
 
-% effictive mass calculation
+% Effictive Mass Calculation
  effective_mass = 294.835;
 
-final_drive_ratio = 2;
+% Final Drive Ratio
+final_drive_ratio = 1/10;
 
 %% Accel Test 
 
-% Redline Trim
+% Trims Engine Data to fit inside rpm bounds (0, redline)
 redline_I = find(engine_torque_data(:,1)>redline);
 engine_torque_data = engine_torque_data(1:redline_I(1),:);
 
-% conversion factors for data
+% Data conversion factors
 rpm_mps = (2*pi()*Rt)/60; % rpm to m/s
 Nm_mps2 = 1/(Rt*effective_mass); % Nm to m/s2
 
-% converts engine torque data to acceleration(m/s2) / velocity(m/s) 
+% Converts engine torque data to acceleration(m/s2) / velocity(m/s) 
 engine_acc_vel = [engine_torque_data(:,1)*rpm_mps, engine_torque_data(:,2)*Nm_mps2];
 
-figure(); hold on
-
-combined_engine_acc_vel = Torque_Curve_Optimizer(engine_acc_vel,engine_gear_ratio,final_drive_ratio,75);
-
+Torque_Curve_Optimizer(engine_acc_vel,engine_gear_ratio,final_drive_ratio,75)
 
--- Final Drive Ratio/Torque_Curve_Optimizer.m
+++ Final Drive Ratio/Torque_Curve_Optimizer.m
@@ -1,15 +1,17 @@
 function [C] = Torque_Curve_Optimizer(engine_acc_vel,engine_gear_ratio,final_drive_ratio,distance)
 
+    % combined_engine_acc_vel = Torque_Curve_Optimizer(engine_acc_vel,engine_gear_ratio,final_drive_ratio,75);
     engine_acc_vel = Gear_Acc_Vel_Curves(engine_acc_vel, engine_gear_ratio, final_drive_ratio);
-
+    
     % lienarize and combine domains for every gear ratio
     combined_engine_acc_vel = [];
-
+    
     for i = 1:length(engine_gear_ratio)
+    
         % Re-fits acc/vel data for gear i to the closest integer domain with a constant step size
-        x = engine_acc_vel(:, 2*i-1); y = engine_acc_vel(:,2*i); 
-        xq = ceil(x(1)):1:floor(x(end)); % New Domain with step 1
-        p = pchip(x, y, xq); % New Data set interpolated over new domain
+        x = engine_acc_vel(:, 2*i-1); y = engine_acc_vel(:,2*i); % pulls data for gear i
+        xq = ceil(x(1)):1:floor(x(end)); % new domain with step 1
+        p = pchip(x, y, xq); % interpolates data for gear i over new domain
         
         % transposes two data sets 
         xq = xq.'; p = p.';
@@ -18,34 +20,38 @@ function [C] = Torque_Curve_Optimizer(engine_acc_vel,engine_gear_ratio,final_dri
         if i == 1
             combined_engine_acc_vel = [xq, p];
         else
-    
-            x = combined_engine_acc_vel(:,1); y = combined_engine_acc_vel(:,2); % placeholder variables
-            int = intersect(x,xq.'); % find intersection of domains
             
-            xqI = [find(xq==int(1)),find(xq==int(end))]; % index range of intersection in xq
-            xI = [find(x==int(1)), find(x==int(end))]; % index range of intersection in x
+            % Checks for intersection between acc/vel curves for gears i, i-1
+            x0 = combined_engine_acc_vel(:,1); y0 = combined_engine_acc_vel(:,2); % gear i-1 data
+            int = intersect(x0,xq); % find intersection of gear domains (velocity both gears can use)
             
-            diff = y(xI(1):xI(end) - p(xqI(1):xqI(end))); % difference between both curves (old - new)
+            xqI = [find(xq==int(1)),find(xq==int(end))]; % index range of intersection in gear i domain
+            x0I = [find(x0==int(1)), find(x0==int(end))]; % index range of intersection in gear i-1 domain
+            
+            diff = y0(x0I(1):x0I(end) - p(xqI(1):xqI(end))); % difference between both curves (old - new)
     
             [min_val, min_I] = min(abs(diff));  % finds the index and value of minimum difference
     
-            if min_val < 0.1 % intersection occurs 
+            if min_val == 0 % intersection criteria
+                disp("INTERSECTION")
+                x0Int = x0I(1) + min_I; % finds index of intersection in gear i-1 domain
+                xqInt = xqI(1) + min_I; % finds index of intersection in gear i domain
                 
-                xI = xI + min_I; % finds index of intersection in x
-                xqI = xqI + min_I; % finds index of intersection in xqI
-    
-                combined_engine_acc_vel = [combined_engine_acc_vel(1:xI), [xq(xqI:end), p(xqI:end)]];
+                % Combines gear i and i-1 data at intersection. Trims right
+                % side of gear i and left side of gear i-1
+                combined_engine_acc_vel = [combined_engine_acc_vel(1:x0Int,:); [xq(xqInt:end), p(xqInt:end)]];
     
             else % intersection does not occur
-    
+                
+                disp("NO INTERSECTION")
+                
+                % Combines gear i and i-1 data at redline. Trims left side of
+                % gear i-1
                 combined_engine_acc_vel = [combined_engine_acc_vel; [xq(xqI(2):end), p(xqI(2):end)]];
             end
         end
     end 
     
-    plot(combined_engine_acc_vel(:,1), combined_engine_acc_vel(:,2))
-
-    
-    
-    C = combined_engine_acc_vel;
+    figure();
+        plot(combined_engine_acc_vel(:,1), combined_engine_acc_vel(:,2))
 end 
