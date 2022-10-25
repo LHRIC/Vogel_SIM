65efaaa41220c3c3a81e6ca35dc19bbfcba2527a
lcos159 <lcos159@gmail.com>
2022-10-24 21:25:31 -0500

    Revert "Revert "Merge branch 'main' of https://github.com/LHRIC/Vogel_SIM""
    This reverts commit fa7f89785616066b09757cbb08569b83833d7997.

--- Functions/vogel.m
+++ Functions/vogel.m
@@ -1,7 +1,7 @@
 function [output] =vogel(x,a,b,Cd,IA_gainf,IA_gainr,twf,KPIf,cg,W,twr,LLTD,rg_r,rg_f, ...
-    casterf,KPIr,deltar,sf_y,T_lock,R,wf,wr,IA_0f,IA_0r)
+    casterf,KPIr,deltar,sf_y,T_lock,R,wf,wr,IA_0f,IA_0r,input)
 
-global grip
+global grip input
         delta = x(1);
         beta = x(2);
         AYP = x(3);
@@ -27,13 +27,13 @@ global grip
         IA_r_in = -twr*sin(phir)/2*IA_gainr - IA_0r - KPIr*(1-cos(deltar)) - casterf*sin(deltar) +phir;
         IA_r_out = -twr*sin(phir)/2*IA_gainr + IA_0r + KPIr*(1-cos(deltar)) - casterf*sin(deltar) + phir; 
         %calculate yaw rate
-        r = A_y*9.81/V;
+        omega = V/R;
         %from yaw, sideslip and steer you can get slip angles (rad)
-        a_f = beta+a*r/V-delta;
-        a_r = beta-b*r/V;
+        a_f = beta+a*omega/V-delta;
+        a_r = beta-b*omega/V;
         % with slip angles, load and camber, calculate lateral force at
         % the front
-        F_fin = -MF52_Fy_fcn([-a_f wfin -IA_f_in])*sf_y*cos(delta); % inputs = (rad Newtons rad)
+        F_fin = MF52_Fy_fcn([a_f wfin -IA_f_in])*sf_y*cos(delta); % inputs = (rad Newtons rad)
         F_fout = MF52_Fy_fcn([a_f wfout -IA_f_out])*sf_y*cos(delta);
         % calculate the drag from aero and the front tires (N)
         F_xDrag = Cd*V^2 + (F_fin+F_fout)*sin(delta)/cos(delta); 
@@ -41,10 +41,11 @@ global grip
         % drag
         rscale = 1-(F_xDrag/W/(polyval(grip,V)))^2; % Comes from traction circle
         % now calculate rear tire forces, with said penalty
-        F_rin = -MF52_Fy_fcn([-a_r wrin -IA_r_in])*sf_y*rscale; 
+        F_rin = MF52_Fy_fcn([a_r wrin -IA_r_in])*sf_y*rscale; 
         F_rout = MF52_Fy_fcn([a_r wrout -IA_r_out])*sf_y*rscale;
         % sum of forces and moments
         F_y = F_fin+F_fout+F_rin+F_rout;
+        f_xplt =(F_fin+F_fout)*sin(delta)/cos(delta)/400;
         M_z_diff = F_xDrag*T_lock*twr/2;       
         M_z = (F_fin+F_fout)*a-(F_rin+F_rout)*b-M_z_diff;
         % calculate resultant lateral acceleration
@@ -52,5 +53,10 @@ global grip
         %minimizing values
         slipAngle = a_f-deg2rad(-12);
         diff_AY =A_y-AY;
+        if input == 1
         output = [M_z slipAngle diff_AY];
+        else
+        output = [A_y f_xplt a_f a_r];
+        end
+
 end
