09178744f0dcd47cbf93023d31a1f9444e0b99b5
jskacines <jskacines@gmail.com>
2024-09-14 11:18:18 -0500

    generate radii set based on track radius density

--- tracks/svg_tracks.py
+++ tracks/svg_tracks.py
@@ -3,16 +3,20 @@ import math
 import svgpathtools as svg
 
 class Trajectory:
-    def __init__(self,track_id,steps,r_min,r_max,scalar):
+    def __init__(self,track_id,steps,track_length,ggv_detail):
         self.steps = steps
         self.radii = []
         self.xpos = []
         self.ypos = []
         self.dL = []
-        self.parse(track_id,scalar)
-        self.calc(steps=steps)
+        self.dL_sum = []
+        self.r_set = []
+        self.parse(track_id,track_length)
+        self.calc(steps=steps,ggv_detail=ggv_detail)
+        
 
-    def parse(self,track_id,scalar):
+    # Read the SVG File
+    def parse(self,track_id,track_length):
         svg_read = svg.svg2paths(track_id)
         svg_path = svg_read[0]
         self.track = svg.Path(svg_path[0])
@@ -25,23 +29,45 @@ class Trajectory:
             self.track=svg.smoothed_path(self.track)
             print('Warning: Non-differentiable Track SVG, Smoothing')
             # Untested
+        # Extract single path from path group
         while len(self.track)==1:
             self.track=self.track[0]
+        # Scale track s.t. length = track length (eg. 2 kilometers for endurance)
+        scalar=(track_length/self.track.length(0,1))
         self.track=self.track.scaled(scalar)
 
-    def calc(self, steps):
+    # Calculate all track parameters
+    def calc(self, steps, ggv_detail):
         for i in range(steps):
             t=i/(steps)
+            # Find path segment index and sub-position
             seg_index, sub_t = self.track.T2t(t)[0],self.track.T2t(t)[1],
-            kappa=svg.path.segment_curvature(self.track[seg_index],t)
+            # Find curvature
+            kappa=svg.path.segment_curvature(self.track[seg_index],sub_t)
+            # Calculate radius
             if kappa==0:
                 self.radii.append(0)
             else:
                 r=(1/kappa)
-                if r>100:
-                    self.radii.append(100)
+                if r>1000:
+                    self.radii.append(1000)
                 else:
                     self.radii.append(1/kappa)
             self.xpos.append(svg.real(self.track.point(t)))
             self.ypos.append(svg.imag(self.track.point(t)))
             self.dL.append(self.track.length(t,t+(1/(steps))))
+            self.dL_sum.append(self.track.length(0,t+(1/(steps))))
+        
+        # Set of Radii for GGV  
+        radii_nonzero = [i for i in self.radii if i != 0]
+        radii_unique = list(set(radii_nonzero))
+        radii_sorted = np.sort(radii_unique)
+        if ggv_detail > len(radii_unique):
+            print('GGV Detail exceeds the number of unique radii:', ggv_detail,'>',len(radii_unique))
+        elif ggv_detail <= 0:
+            print('GGV Detail must be greater than or equal to 1')
+        else:
+            for i in range(ggv_detail):
+                n = (int)(i*(len(radii_sorted)/(ggv_detail)))
+                self.r_set.append(radii_sorted[n])
+        print('radii count', len(self.r_set))
--- tracks/test.py
+++ tracks/test.py
@@ -1,12 +1,27 @@
 import svg_tracks
 import matplotlib.pyplot as plt
 from matplotlib import cm
-t=svg_tracks.Trajectory(track_id='tracktest.svg',steps=1000,r_min=0,r_max=100,scalar=0.1)
+# t1=svg_tracks.Trajectory(track_id='2023_IC_michigan_endurance.svg',steps=1000,r_min=0,r_max=100,scalar=0.1,track_length=2000)
+t2=svg_tracks.Trajectory(track_id='2019_IC_michigan_endurance.svg',\
+                         steps=1000,track_length=2000,ggv_detail=100)
 
 fig3, ax3 = plt.subplots()
-sc = ax3.scatter(t.xpos, t.ypos, c=t.radii)
+sc = ax3.scatter(t2.xpos, t2.ypos, c=t2.radii)
 ax3.axis("equal")
 cbar = plt.colorbar(sc)
 cbar.set_label("Radii")
 
+fig5, ax5 = plt.subplots()
+sc = ax5.scatter(list(range(len(t2.r_set))),t2.r_set)
+plt.xlabel("Num")
+plt.ylabel("Radii")
+cbar = plt.colorbar(sc)
+cbar.set_label("Radii")
 plt.show()
+
+# fig4, ax4 = plt.subplots()
+# sc = ax4.scatter(t1.xpos, t1.ypos, c=t1.radii)
+# ax4.axis("equal")
+# cbar = plt.colorbar(sc)
+# cbar.set_label("Radii")
+# plt.show()
